e3b19fa0b0f173a5a2eccdceae5865a378eafb48
Peter Story <peter.story@melroseschools.com>
2020-01-29 18:53:04 -0500

    I got the encoder values to work in neos

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.CANEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -17,27 +18,42 @@ public class DriveSubsystem extends SubsystemBase {
 
   /*Creates motors, getting motor controller (CANSparkMax) ports from RobotMap
   * MAKE SURE TEST BED IS SET TO BRUSHED*/
-  public final CANSparkMax frontLeft = new CANSparkMax(RobotMap.frontLeftTalonPort, CANSparkMaxLowLevel.MotorType.kBrushed);
-  private final CANSparkMax backLeft = new CANSparkMax(RobotMap.backLeftTalonPort, CANSparkMaxLowLevel.MotorType.kBrushed);
-  private final CANSparkMax frontRight = new CANSparkMax(RobotMap.frontRightTalonPort, CANSparkMaxLowLevel.MotorType.kBrushed);
-  private final CANSparkMax backRight = new CANSparkMax(RobotMap.backRightTalonPort, CANSparkMaxLowLevel.MotorType.kBrushed);
-
+  public final CANSparkMax frontLeft = new CANSparkMax(RobotMap.frontLeftTalonPort, CANSparkMaxLowLevel.MotorType.kBrushless);
+  private final CANSparkMax backLeft = new CANSparkMax(RobotMap.backLeftTalonPort, CANSparkMaxLowLevel.MotorType.kBrushless);
+  private final CANSparkMax frontRight = new CANSparkMax(RobotMap.frontRightTalonPort, CANSparkMaxLowLevel.MotorType.kBrushless);
+  private final CANSparkMax backRight = new CANSparkMax(RobotMap.backRightTalonPort, CANSparkMaxLowLevel.MotorType.kBrushless);
+  private CANEncoder encoder1 = frontLeft.getEncoder();
+  private CANEncoder encoder2 = backLeft.getEncoder();
+  private CANEncoder encoder3 = frontRight.getEncoder();
+  private CANEncoder encoder4 = backRight.getEncoder();
   //Differential drive coordinates motors, used for tank + arcade drive
   public SMDrive driveCommand = new SMDrive(this);
   public DifferentialDrive roboDrive = new DifferentialDrive(frontLeft, frontRight);
 
   public DriveSubsystem() {
     Robot.initializeSparkDefaults(frontLeft, frontRight);
+
     backLeft.follow(frontLeft);
     backRight.follow(frontRight);
 
     roboDrive.setDeadband(RobotMap.DEADBAND);
     setDefaultCommand(driveCommand);
 
+
+
+
   }
 
   @Override
   public void periodic() {
+    double value = encoder1.getPosition();
+    double value2 = encoder2.getPosition();
+    double value3 = encoder3.getPosition();
+    double value4 = encoder4.getPosition();
+    System.out.println("The value of encoder 1 is: " + value);
+    System.out.println("The value of encoder 2 is: " + value2);
+    System.out.println("The value of encoder 3 is: " + value3);
+    System.out.println("The value of encoder 4 is: " + value4);
     // This method will be called once per scheduler run
   }
 
