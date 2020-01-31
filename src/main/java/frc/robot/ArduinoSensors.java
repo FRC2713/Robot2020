2d98b05c6af204505c04d738863826285293eb29
Marissa Zelten <mmzelten@gmail.com>
2020-01-30 19:22:55 -0500

    Created class ArduinoSensors

---
+++ src/main/java/frc/robot/ArduinoSensors.java
@@ -0,0 +1,45 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class ArduinoSensors  {
+  private static ArduinoSensors defaultInstance = null;
+
+  public static ArduinoSensors getInstance() {
+    if(defaultInstance == null)
+    {
+      defaultInstance = new AdruinoSensors();
+    }
+    return defaultInstance;
+  }
+
+  private SerialPort port = new SerialPort(9600, SerialPort.Port.kUSB);
+  public double LRFinches = 0;
+  public ArduinoSensors() {
+    port.enableTermination();
+
+  }
+
+  public void execute() {
+    String str = null;
+    str = port.readString().trim();
+
+    switch (str) {
+
+      case "LRF":
+
+
+        try {
+          LRFinches = Double.parseDouble(port.readString().trim()) * 0.0393701;
+        } catch (NumberFormatException e) {
+          LRFinches = -1;
+
+        }
+        if (LRFinches != -1) System.out.println("This is the distance: " + LRFinches);
+
+        break;
+    }
+  }
+
+}
