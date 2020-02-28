2e0ea273a849ddef938659310d92bf2c792506b6
Sammy Mounsif <sammy.mounsif@melroseschools.com>
2020-02-28 18:36:57 -0500

    Just added those commands I talked about in the last push

---
+++ src/main/java/frc/robot/commands/commandGroups/scoreFront.java
@@ -0,0 +1,25 @@
+package frc.robot.commands.commandGroups;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.IntakeGateCommand;
+import frc.robot.commands.moveCommands.AutonomousCommand;
+import frc.robot.commands.moveCommands.moveBackwards10Feet;
+import frc.robot.commands.moveCommands.moveForward15Feet;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+
+public class scoreFront extends SequentialCommandGroup {
+  public scoreFront(DriveSubsystem driveSubsystem) {
+    addCommands(
+
+
+      new moveBackwards10Feet(driveSubsystem),
+
+//open gate
+
+      new moveForward15Feet(driveSubsystem)
+
+//close gate
+    );
+  }
+}
