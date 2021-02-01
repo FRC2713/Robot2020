fd23f9bc644eba7cd2f6e153819e3c4d9ec7e13f
Sammy Mounsif <sammy.mounsif@melroseschools.com>
2021-02-01 18:35:20 -0500

    Created a new folder for commands for this year. Created 4 new AutonomousCommand classes, which do the same things our last commands did (encoder-based moving and turning), but make it clearer which ones to use. Also made the code itself slightly more flexible.

--- src/main/java/frc/robot/commands/commandGroups/initLineOnly.java
+++ src/main/java/frc/robot/commands/commandGroups/initLineOnly.java
@@ -1,9 +1,9 @@
 package frc.robot.commands.commandGroups;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.moveCommands.AutonomousCommand;
-import frc.robot.commands.moveCommands.turnLeft45;
-import frc.robot.commands.moveCommands.turnRight45;
+import frc.robot.commands.oldMoveCommands.AutonomousCommand;
+import frc.robot.commands.oldMoveCommands.turnLeft45;
+import frc.robot.commands.oldMoveCommands.turnRight45;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class initLineOnly extends SequentialCommandGroup {
--- src/main/java/frc/robot/commands/commandGroups/newScoreLeft.java
+++ src/main/java/frc/robot/commands/commandGroups/newScoreLeft.java
@@ -1,10 +1,10 @@
 package frc.robot.commands.commandGroups;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.moveCommands.moveBackward;
-import frc.robot.commands.moveCommands.moveForward;
-import frc.robot.commands.moveCommands.turnLeft45;
-import frc.robot.commands.moveCommands.turnRight45;
+import frc.robot.commands.oldMoveCommands.moveBackward;
+import frc.robot.commands.oldMoveCommands.moveForward;
+import frc.robot.commands.oldMoveCommands.turnLeft45;
+import frc.robot.commands.oldMoveCommands.turnRight45;
 import frc.robot.commands.outputGateCommand;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
