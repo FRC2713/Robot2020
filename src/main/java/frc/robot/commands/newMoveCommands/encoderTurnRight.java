a8b028a65e01f1b1d90f294b7d72b0ab84cd768d
Sammy Mounsif <sammy.mounsif@melroseschools.com>
2021-02-01 18:38:09 -0500

    Created 4 new AutonomousCommand classes, which do the same things our last commands did (encoder-based moving and turning), but make it clearer which ones to use. Also made the code itself slightly more flexible.

---
+++ src/main/java/frc/robot/commands/newMoveCommands/encoderTurnRight.java
@@ -0,0 +1,55 @@
+package frc.robot.commands.newMoveCommands;
+
+import com.revrobotics.CANEncoder;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class encoderTurnRight extends CommandBase {
+  double originalDist = 0;
+  double newDist = 0;
+  double accumulatedDist = 0;
+  double leftSpeed = 0;
+  double rightSpeed = 0;
+  double targetAngle = 0;
+  CANEncoder encoder1;
+
+  private final DriveSubsystem m_driveSubsystem;
+
+  public encoderTurnRight(double angle, DriveSubsystem driveSubsystem) {
+    m_driveSubsystem = driveSubsystem;
+    targetAngle = angle;
+    addRequirements(driveSubsystem); //establishes drive subsystem as subsystem used
+    accumulatedDist = 0;
+  }
+
+  @Override
+  public void initialize() {
+    encoder1 = m_driveSubsystem.getEncoder(1);
+    originalDist = m_driveSubsystem.toFeet(encoder1.getPosition());
+    newDist = originalDist;
+    m_driveSubsystem.resetEncoder(encoder1);
+  }
+
+  @Override
+  public void execute() {
+    System.out.println("Execute loop");
+    leftSpeed = 0.5;
+    rightSpeed = -0.5;
+    m_driveSubsystem.getRoboDrive().tankDrive(leftSpeed, rightSpeed);
+    newDist = encoder1.getPosition();
+    accumulatedDist += m_driveSubsystem.toFeet(m_driveSubsystem.encoderDistance(encoder1)); //adds old distance to encoder input, translated to feet;
+    System.out.println("Should be moving right now");
+    System.out.println("Traveled " + accumulatedDist + " Feet");
+  }
+
+  @Override
+  public boolean isFinished() {
+    if (accumulatedDist > targetAngle/45) { //Guess-and-checked number
+      m_driveSubsystem.getRoboDrive().stopMotor();
+      Timer.delay(0.25);
+      return true;
+    }
+    else return false;
+  }
+}
