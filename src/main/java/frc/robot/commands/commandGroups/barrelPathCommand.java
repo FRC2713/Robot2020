536097bf9466c048ab7ac449253de5abae82afe0
Aidan McCarthy <aidanjmc@gmail.com>
2021-02-12 19:41:16 -0500

    Created commandGroups for barrel path and slalom path

---
+++ src/main/java/frc/robot/commands/commandGroups/barrelPathCommand.java
@@ -0,0 +1,58 @@
+package frc.robot.commands.commandGroups;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class barrelPathCommand extends SequentialCommandGroup {
+  public barrelPathCommand(DriveSubsystem driveSubsystem){
+     /*addCommands(
+
+      new goForward(42.67/12, drivesubsystem),
+
+      new encoderTurnRight(60, drivesubsystem),
+
+      new goForward(64.64/12, drivesubsystem),
+
+      new encoderTurnLeft(60, drivesubsystem),
+
+      new goForward(150.0/12, drivesubsystem),
+
+      new encoderTurnLeft(60, drivesubsystem),
+
+      new goForward(60.0/12, drivesubsystem),
+
+      new encoderTurnRight(60, drivesubsystem),
+
+      new goForward(30.0/12, drivesubsystem),
+
+      new encoderTurnRight(60, drivesubsystem),
+
+      new goForward(30.0/12, drivesubsystem),
+
+      new encoderTurnRight(60, drivesubsystem),
+
+      new goForward(30.0/12, drivesubsystem),
+
+      new encoderTurnRight(60, drivesubsystem),
+
+      new goForward(30.0/12, drivesubsystem),
+
+      new encoderTurnRight(60, drivesubsystem),
+
+      new goForward(60.0/12, drivesubsystem),
+
+      new encoderTurnLeft(60, drivesubsystem),
+
+      new goForward(150.0/12, drivesubsystem),
+
+      new encoderTurnLeft(60, drivesubsystem),
+
+      new goForward(64.641/12, drivesubsystem),
+
+      new encoderTurnRight(60, drivesubsystem),
+
+      new goForward(42.67/12, drivesubsystem)
+
+    );*/
+  }
+}
---
+++ src/main/java/frc/robot/commands/commandGroups/slalomPathCommand.java
@@ -0,0 +1,44 @@
+package frc.robot.commands.commandGroups;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.DriveSubsystem;
+
+public class slalomPathCommand extends SequentialCommandGroup {
+  public slalomPathCommand(DriveSubsystem driveSubsystem) {
+    /*addCommands(
+
+      new encoderTurnLeft(45, drivesubsystem),
+
+      new goForward(76.011/12, drivesubsystem),
+
+      new encoderTurnRight(45, drivesubsystem),
+
+      new goForward(132.504/12, drivesubsystem),
+
+      new encoderTurnRight(45, drivesubsystem),
+
+      new goForward(76.011/12, drivesubsystem),
+
+      new encoderTurnLeft(90, drivesubsystem),
+
+      new goForward(42.426/12, drivesubsystem),
+
+      new encoderTurnLeft(90, drivesubsystem),
+
+      new goForward(42.426/12, drivesubsystem),
+
+      new encoderTurnLeft(90, drivesubsystem),
+
+      new goForward(75.06/12, drivesubsystem),
+
+      new encoderTurnRight(45, drivesubsystem),
+
+      new goForward(133.849/12, drivesubsystem),
+
+      new encoderTurnRight(45, drivesubsystem),
+
+      new goForward(75.08/12, drivesubsystem)
+    );*/
+
+  }
+}
