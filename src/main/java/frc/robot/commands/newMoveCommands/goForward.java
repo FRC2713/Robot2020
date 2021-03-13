1d90bcbba08b51456fbf3f75efd039748e7e471b
Sammy Mounsif <sammy.mounsif@melroseschools.com>
2021-03-12 19:21:10 -0500

    Added slew code. I don't care if it's technically not slew code. I'm calling it slew code. If you don't like it write your own It's-A-Little-More-Complicated-Than-Slew-Code code >:(

--- src/main/java/frc/robot/commands/newMoveCommands/goForward.java
+++ src/main/java/frc/robot/commands/newMoveCommands/goForward.java
@@ -7,9 +7,11 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class goForward extends CommandBase {
-  double originalDist = 0;
-  double newDist = 0;
+//  double originalDist = 0;
+//  double newDist = 0;
   double accumulatedDist = 0;
+  final double ACCEL_CONSTANT = 0.02; //0.02 will bring speed from 0 to 1 in 1 second; scales linearly
+  double SLEW_DIST = 0; //Determines how much distance the robot is given to slow down, in feet - will always be equal to the time taken to speed up.
   double leftSpeed = 0;
   double rightSpeed = 0;
   double targetDist = 0;
@@ -28,18 +30,38 @@ public class goForward extends CommandBase {
   @Override
   public void initialize() {
     encoder1 = m_DS.getEncoder(1);
-    originalDist = m_DS.toFeet(encoder1.getPosition());
-    newDist = originalDist;
+//    originalDist = m_DS.toFeet(encoder1.getPosition());
+//    newDist = originalDist;
     m_DS.resetEncoder(encoder1);
   }
 
   @Override
-  public void execute() {
-    leftSpeed = 1;
-    rightSpeed = 1;
-    m_DS.getRoboDrive().tankDrive(leftSpeed, rightSpeed);
-    newDist = encoder1.getPosition();
+  public void execute()
+  {
+//    newDist = encoder1.getPosition();
     accumulatedDist = Math.abs(m_DS.improvedEncoderDist(encoder1));
+    if (accumulatedDist < targetDist-SLEW_DIST)
+    {
+      if (leftSpeed < 1)
+      {
+        leftSpeed += ACCEL_CONSTANT;
+        rightSpeed += ACCEL_CONSTANT;
+        SLEW_DIST = accumulatedDist;
+      }
+    }
+    else
+      {
+      if(leftSpeed > ACCEL_CONSTANT)
+        {
+        leftSpeed -= ACCEL_CONSTANT;
+        rightSpeed -= ACCEL_CONSTANT;
+        }
+      else
+        {
+        accumulatedDist = targetDist + 1;
+        }
+      }
+    m_DS.getRoboDrive().tankDrive(leftSpeed, rightSpeed);
     System.out.println("Traveled " + accumulatedDist + " Feet");
   }
 
--- src/main/java/frc/robot/commands/newMoveCommands/gyroTurnLeft.java
+++ src/main/java/frc/robot/commands/newMoveCommands/gyroTurnLeft.java
@@ -9,8 +9,10 @@ public class gyroTurnLeft extends CommandBase {
   private double targetAngle;
   private double currentAngle;
   private double originalAngle;
-  private double rightSpeed;
-  private double leftSpeed;
+  final double ACCEL_CONSTANT = 0.02;
+  double SLEW_DIST = 0;
+  private double rightSpeed = 0;
+  private double leftSpeed = 0;
   ADXRS450_Gyro gyro;
 
   private final DriveSubsystem m_DS;
@@ -33,10 +35,24 @@ public class gyroTurnLeft extends CommandBase {
 
   @Override
   public void execute() {
-    leftSpeed = -1;
-    rightSpeed = 1;
-    m_DS.getRoboDrive().tankDrive(leftSpeed, rightSpeed);
     currentAngle = gyro.getAngle() - originalAngle;
+    if (currentAngle < targetAngle-SLEW_DIST) {
+      if(leftSpeed > -1) {
+        leftSpeed -= ACCEL_CONSTANT;
+        rightSpeed += ACCEL_CONSTANT;
+        SLEW_DIST = currentAngle;
+      }
+    }
+    else {
+      if(leftSpeed > ACCEL_CONSTANT) {
+        leftSpeed += ACCEL_CONSTANT;
+        rightSpeed -= ACCEL_CONSTANT;
+      }
+      else {
+        currentAngle = targetAngle + 1;
+      }
+    }
+    m_DS.getRoboDrive().tankDrive(leftSpeed, rightSpeed);
     System.out.println("Turned " + currentAngle + " degrees left");
   }
 
