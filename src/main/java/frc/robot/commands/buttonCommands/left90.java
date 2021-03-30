c9c45c1801e6f096d7eb8c35f742f122a2ed43eb
BigBigB7 <brennan.cesar@gmail.com>
2021-03-29 20:27:37 -0400

    Small changes; created new class with minimal code to house the code which will turn the robot 90 degrees left with a button press.

---
+++ src/main/java/frc/robot/commands/buttonCommands/left90.java
@@ -0,0 +1,19 @@
+//class is for command to turn the robot 90 degrees left
+
+package frc.robot.commands.buttonCommands;
+
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveSubsystem;
+
+
+public class left90 extends CommandBase {
+  ADXRS450_Gyro gyro;
+  private double turnFactor = 90;
+
+  public void left90Button() {
+//turn code goes here
+
+  }
+
+}
