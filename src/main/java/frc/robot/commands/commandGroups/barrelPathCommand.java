d9ae7f6f0fcadef9f422b92ad0dcbf45bf85088c
BigBigB7 <brennan.cesar@gmail.com>
2021-03-31 18:54:35 -0400

    Fixed slalom path autonomous; all three are now theoretically functioning.

--- src/main/java/frc/robot/commands/commandGroups/barrelPathCommand.java
+++ src/main/java/frc/robot/commands/commandGroups/barrelPathCommand.java
@@ -4,60 +4,62 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.newMoveCommands.*;
 import frc.robot.subsystems.DriveSubsystem;
 
-
 public class barrelPathCommand extends SequentialCommandGroup {
-  public barrelPathCommand(DriveSubsystem drivesubsystem){
-     addCommands(
-//Tina Chen Barrel Path
-       new goForward(90/12, drivesubsystem),
+  public barrelPathCommand(DriveSubsystem drivesubsystem) {
 
-       new gyroTurnRight(45, drivesubsystem),
+    //barrel path
+    addCommands(
 
-       new goForward(42.426/12, drivesubsystem),
+      new goForward(90/12, drivesubsystem),
 
-       new gyroTurnRight(90, drivesubsystem),
+      new gyroTurnRight(45, drivesubsystem),
 
-       new goForward(42.426/12, drivesubsystem),
+      new goForward(42.426/12, drivesubsystem),
 
-       new gyroTurnRight(90, drivesubsystem),
+      new gyroTurnRight(90, drivesubsystem),
 
-       new goForward(42.426/12, drivesubsystem),
+      new goForward(42.426/12, drivesubsystem),
 
-       new gyroTurnRight(90, drivesubsystem),
+      new gyroTurnRight(90, drivesubsystem),
 
-       new goForward(42.426/12, drivesubsystem),
+      new goForward(42.426/12, drivesubsystem),
 
-       new gyroTurnRight(45, drivesubsystem),
+      new gyroTurnRight(90, drivesubsystem),
 
-       new goForward(90/12, drivesubsystem),
+      new goForward(42.426/12, drivesubsystem),
 
-       new gyroTurnLeft(45, drivesubsystem),
+      new gyroTurnRight(45, drivesubsystem),
 
-       new goForward(42.426/12, drivesubsystem),
+      new goForward(90/12, drivesubsystem),
 
-       new gyroTurnLeft(90, drivesubsystem),
+      new gyroTurnLeft(45, drivesubsystem),
 
-       new goForward(42.426/12, drivesubsystem),
+      new goForward(42.426/12, drivesubsystem),
 
-       new gyroTurnLeft(90, drivesubsystem),
+      new gyroTurnLeft(90, drivesubsystem),
 
-       new goForward(42.426/12, drivesubsystem),
+      new goForward(42.426/12, drivesubsystem),
 
-       new gyroTurnLeft(90, drivesubsystem),
+      new gyroTurnLeft(90, drivesubsystem),
 
-       new goForward(127.279/12, drivesubsystem),
+      new goForward(42.426/12, drivesubsystem),
 
-       new gyroTurnLeft(90, drivesubsystem),
+      new gyroTurnLeft(90, drivesubsystem),
 
-       new goForward(42.426/12, drivesubsystem),
+      new goForward(127.279/12, drivesubsystem),
 
-       new gyroTurnLeft(90, drivesubsystem),
+      new gyroTurnLeft(90, drivesubsystem),
 
-       new goForward(42.426/12, drivesubsystem),
+      new goForward(42.426/12, drivesubsystem),
 
-       new gyroTurnLeft(45, drivesubsystem),
+      new gyroTurnLeft(90, drivesubsystem),
 
-       new goForward(240/12, drivesubsystem)
+      new goForward(42.426/12, drivesubsystem),
+
+      new gyroTurnLeft(45, drivesubsystem),
+
+      new goForward(240/12, drivesubsystem)
+
      );
   }
 }
--- src/main/java/frc/robot/commands/commandGroups/slalomPathCommand.java
+++ src/main/java/frc/robot/commands/commandGroups/slalomPathCommand.java
@@ -1,44 +1,47 @@
 package frc.robot.commands.commandGroups;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.newMoveCommands.*;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class slalomPathCommand extends SequentialCommandGroup {
-  public slalomPathCommand(DriveSubsystem driveSubsystem) {
-    /*addCommands(
+  public slalomPathCommand(DriveSubsystem drivesubsystem) {
 
-      new encoderTurnLeft(45, drivesubsystem),
+    //slalom path
+    addCommands(
+
+      new gyroTurnLeft(45, drivesubsystem),
 
       new goForward(76.011/12, drivesubsystem),
 
-      new encoderTurnRight(45, drivesubsystem),
+      new gyroTurnRight(45, drivesubsystem),
 
       new goForward(132.504/12, drivesubsystem),
 
-      new encoderTurnRight(45, drivesubsystem),
+      new gyroTurnRight(45, drivesubsystem),
 
       new goForward(76.011/12, drivesubsystem),
 
-      new encoderTurnLeft(90, drivesubsystem),
+      new gyroTurnLeft(90, drivesubsystem),
 
       new goForward(42.426/12, drivesubsystem),
 
-      new encoderTurnLeft(90, drivesubsystem),
+      new gyroTurnLeft(90, drivesubsystem),
 
       new goForward(42.426/12, drivesubsystem),
 
-      new encoderTurnLeft(90, drivesubsystem),
+      new gyroTurnLeft(90, drivesubsystem),
 
       new goForward(75.06/12, drivesubsystem),
 
-      new encoderTurnRight(45, drivesubsystem),
+      new gyroTurnRight(45, drivesubsystem),
 
       new goForward(133.849/12, drivesubsystem),
 
-      new encoderTurnRight(45, drivesubsystem),
+      new gyroTurnRight(45, drivesubsystem),
 
       new goForward(75.08/12, drivesubsystem)
-    );*/
+    );
 
   }
 }
